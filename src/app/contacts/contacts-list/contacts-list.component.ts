0d4b5333e0f83b24f556c019c3b100f3ba16c517
Zosia Samosia <Zosia Samosia>
2023-05-25 13:42:14 +0200

    asadsadsadsa

--- src/app/contacts/contacts-list/contacts-list.component.ts
+++ src/app/contacts/contacts-list/contacts-list.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./contacts-list.component.scss']
 })
 export class ContactsListComponent {
+  //private is only for this class
+  //protected goes for structures like html
+  //public allows to use all of that and in other scripts
+  protected contacts: any = [
+    {id: 1, surname: "Nowak", firstName: "Tomek", phone: "+48 111222333"}
+  ];
 
 }
